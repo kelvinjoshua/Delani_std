4c40d0c49422f8be8e0dcc863b284ada35f5d3d0
kelvinjoshua <kelvinbale78@gmail.com>
2020-02-24 17:44:24 +0300

    last changes

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ $(".image-text").click(function(){
 });
 });
 
+$(document).ready(function(){
+    $(".image-text2").click(function(){
+        $(".image2").toggle();
+        $(".text2").toggle();
+    });
+    });
+
+    $(document).ready(function(){
+        $(".image-text3").click(function(){
+            $(".image3").toggle();
+            $(".text3").toggle();
+        });
+        });
+/*  */
+$("#submit").click(function (event) {
+    event.preventDefault();
+    var user = $("#name").val();
+    var message = $("email").val();
+    var area = $("#random").val();
+    //var key = '43ab10333d46e19325f96d210ed40c49-us5';//
+    if (user == "" || message == "" || area == "") {
+        alert(user + " we have recieved your submission")        } else {
+        alert("fill out fields")        }    })
